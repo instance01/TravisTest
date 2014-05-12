82f3e4e9ba218407de3c31c97b7c4836b8746929
InstanceLabs <danielratke97@hotmail.de>
2014-05-12 17:45:07 +0200

    final test

--- 1_7_R3/src/main/java/com/comze_instancelabs/minigamesapi/effects/Effects.java
+++ 1_7_R3/src/main/java/com/comze_instancelabs/minigamesapi/effects/Effects.java
@@ -1,9 +1,5 @@
 package com.comze_instancelabs.minigamesapi.effects;
 
-import com.comze_instancelabs.minigamesapi.MinigamesAPI;
-
 public class Effects {
-	public void test(){
-		MinigamesAPI.getAPI();
-	}
+	
 }
